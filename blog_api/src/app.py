0f39dd49a58cc0d7e152d981731b038f3a98d150
karan kaul <kk629@njit.edu>
2019-12-17 18:55:16 -0500

    Feature:- imported db and bcrypt from src/model and initialized it

--- blog_api/src/app.py
+++ blog_api/src/app.py
@@ -21,3 +21,33 @@ def create_app(env_name):
         return 'Congratulations! Your first endpoint is workin'
 
     return app
+
+
+# src/app.py
+
+from flask import Flask
+
+from .config import app_config
+from .models import db, bcrypt  # add this new line
+
+
+def create_app(env_name):
+    """
+    Create app
+    """
+
+    # app initiliazation
+    app = Flask(__name__)
+
+    app.config.from_object(app_config[env_name])
+
+    # initializing bcrypt
+    bcrypt.init_app(app)  # add this line
+
+    db.init_app(app)  # add this line
+
+    #####################
+    # existing code remain #
+    ######################
+
+    return app
