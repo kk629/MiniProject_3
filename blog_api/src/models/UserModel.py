577f73d681907b47309aa0e3889fb1a5a27b4510
karan kaul <kk629@njit.edu>
2019-12-17 18:39:52 -0500

    Feature:- Made changes to UserModel to allow hashing of user's password,

--- blog_api/src/models/UserModel.py
+++ blog_api/src/models/UserModel.py
@@ -2,8 +2,8 @@
 
 from marshmallow import fields, Schema
 import datetime
-from . import db
-
+from . import db, bcrypt
+from ..app import bcrypt
 
 class UserModel(db.Model):
     """
@@ -55,3 +55,58 @@ class UserModel(db.Model):
 
     def __repr(self):
         return '<id {}>'.format(self.id)
+
+
+
+
+
+class UserModel(db.Model):
+        """
+        User Model
+        """
+
+        # table name
+        __tablename__ = 'users'
+
+        id = db.Column(db.Integer, primary_key=True)
+
+        #####################
+        # existing code remain #
+        ######################
+
+        # class constructor
+        def __init__(self, data):
+            """
+            Class constructor
+            """
+            self.name = data.get('name')
+            self.email = data.get('email')
+            self.password = self.__generate_hash(data.get('password'))  # add this line
+
+        #####################
+        # existing code remain #
+        ######################
+
+        def update(self, data):
+            for key, item in data.items():
+                if key == 'password':  # add this new line
+                    self.password = self.__generate_hash(value)  # add this new line
+                setattr(self, key, item)
+            self.modified_at = datetime.datetime.utcnow()
+            db.session.commit()
+
+        #####################
+        # existing code remain #
+        ######################
+
+        # add this new method
+        def __generate_hash(self, password):
+            return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
+
+        # add this new method
+        def check_hash(self, password):
+            return bcrypt.check_password_hash(self.password, password)
+
+        #####################
+        # existing code remain #
+        ######################
