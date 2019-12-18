cfe56f0db95d223285d30a557bc41d4a0528be72
karan kaul <kk629@njit.edu>
2019-12-17 19:16:07 -0500

    Feature:- included a new method get_all endpoint

--- blog_api/src/UserView.py
+++ blog_api/src/UserView.py
@@ -88,3 +88,49 @@ def login():
     token = Auth.generate_token(ser_data.get('id'))
 
     return custom_response({'jwt_token': token}, 200)
+
+
+from flask import request, json, Response, Blueprint
+
+
+user_api = Blueprint('user_api', __name__)
+user_schema = UserSchema()
+
+
+@user_api.route('/', methods=['POST'])
+def create():
+    """
+    Create User Function
+    """
+    #####################
+    # existing code remain #
+    ######################
+    return custom_response({'jwt_token': token}, 201)
+
+
+# add this new method
+@user_api.route('/', methods=['GET'])
+@Auth.auth_required
+def get_all():
+    users = UserModel.get_all_users()
+    ser_users = user_schema.dump(users, many=True).data
+    return custom_response(ser_users, 200)
+
+
+@user_api.route('/login', methods=['POST'])
+def login():
+    #####################
+    # existing code remain #
+    ######################
+    return custom_response({'jwt_token': token}, 200)
+
+
+def custom_response(res, status_code):
+    """
+    Custom Response Function
+    """
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
