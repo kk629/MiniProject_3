4f77e6613528bd4dbdccd77162b469629a18c130
karan kaul <kk629@njit.edu>
2019-12-17 19:24:11 -0500

    Feature:- Did registration

--- blog_api/src/app.py
+++ blog_api/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, app
 
 from .config import app_config
 
@@ -23,11 +23,7 @@ def create_app(env_name):
     return app
 
 
-# src/app.py
 
-from flask import Flask
-
-from .config import app_config
 from .models import db, bcrypt  # add this new line
 
 
@@ -49,5 +45,36 @@ def create_app(env_name):
     #####################
     # existing code remain #
     ######################
+
+    return app
+
+
+from flask import Flask
+
+from .config import app_config
+from .models import db, bcrypt
+
+# import user_api blueprint
+from .views.UserView import user_api as user_blueprint
+
+
+def create_app(env_name):
+    """
+    Create app
+    """
+
+    # app initiliazation
+    #####################
+    # existing code remain #
+    ######################
+
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')  # add this line
+
+    @app.route('/', methods=['GET'])
+    def index():
+        """
+        example endpoint
+        """
+        return 'Congratulations! Your first endpoint is working'
 
     return app
