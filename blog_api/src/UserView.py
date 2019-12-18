ff1a8c179e748026d8f0f1a509da2ebb3835a237
karan kaul <kk629@njit.edu>
2019-12-17 19:18:46 -0500

    Feature:- added 4 more end points

--- blog_api/src/UserView.py
+++ blog_api/src/UserView.py
@@ -90,7 +90,7 @@ def login():
     return custom_response({'jwt_token': token}, 200)
 
 
-from flask import request, json, Response, Blueprint
+from flask import request, json, Response, Blueprint,g
 
 
 user_api = Blueprint('user_api', __name__)
@@ -134,3 +134,69 @@ def custom_response(res, status_code):
         response=json.dumps(res),
         status=status_code
     )
+
+
+
+user_api = Blueprint('user_api', __name__)
+user_schema = UserSchema()
+
+
+#####################
+# existing code remains #
+########################
+
+@user_api.route('/<int:user_id>', methods=['GET'])
+@Auth.auth_required
+def get_a_user(user_id):
+    """
+    Get a single user
+    """
+    user = UserModel.get_one_user(user_id)
+    if not user:
+        return custom_response({'error': 'user not found'}, 404)
+
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/me', methods=['PUT'])
+@Auth.auth_required
+def update():
+    """
+    Update me
+    """
+    req_data = request.get_json()
+    data, error = user_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+
+    user = UserModel.get_one_user(g.user.get('id'))
+    user.update(data)
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/me', methods=['DELETE'])
+@Auth.auth_required
+def delete():
+    """
+    Delete a user
+    """
+    user = UserModel.get_one_user(g.user.get('id'))
+    user.delete()
+    return custom_response({'message': 'deleted'}, 204)
+
+
+@user_api.route('/me', methods=['GET'])
+@Auth.auth_required
+def get_me():
+    """
+    Get me
+    """
+    user = UserModel.get_one_user(g.user.get('id'))
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+#####################
+# existing code remains #
+########################
