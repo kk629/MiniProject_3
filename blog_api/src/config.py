8c358dc9fd4766784b7a7798b52a84e879dc0ae5
karan kaul <kk629@njit.edu>
2019-12-18 17:11:56 -0500

    Feature:- Created tests directory and added python unit test in test_user.py

--- blog_api/src/config.py
+++ blog_api/src/config.py
@@ -22,3 +22,19 @@ app_config = {
     'development': Development,
     'production': Production,
 }
+
+class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+ app_config = {
+  #####################
+    # existing code here#
+    #####################
+    'testing': Testing
+ }
