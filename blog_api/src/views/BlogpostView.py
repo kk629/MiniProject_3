b7179f282a2fd76e5b499528ecb52a472d4dc6e3
karan kaul <kk629@njit.edu>
2019-12-18 16:57:31 -0500

    Feature:- Added a deleting feature

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -121,3 +121,37 @@ def update(blogpost_id):
 
     data = blogpost_schema.dump(post).data
     return custom_response(data, 200)
+
+@blogpost_api.route('/', methods=['POST'])
+@Auth.auth_required
+def create():
+  """
+  Create Blogpost Function
+  """
+  # app initiliazation
+  #####################
+  # existing code remain #
+  ######################
+  return custom_response(data, 201)
+
+# app initiliazation
+#####################
+# existing code remain #
+######################
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['DELETE'])
+@Auth.auth_required
+def delete(blogpost_id):
+  """
+  Delete A Blogpost
+  """
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  if data.get('owner_id') != g.user.get('id'):
+    return custom_response({'error': 'permission denied'}, 400)
+
+  post.delete()
+  return custom_response({'message': 'deleted'}, 204)
