4c575b79b9daf25ad8b5f37ec1e370bb2635c188
karan kaul <kk629@njit.edu>
2019-12-18 16:50:25 -0500

    Feature:- Added a new function

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -32,3 +32,29 @@ def custom_response(res, status_code):
     response=json.dumps(res),
     status=status_code
   )
+
+
+def data(args):
+    pass
+
+
+@blogpost_api.route('/', methods=['POST'])
+@Auth.auth_required
+def create():
+  """
+  Create Blogpost Function
+  """
+  
+  return custom_response(data, 201)
+
+# add this function
+@blogpost_api.route('/', methods=['GET'])
+def get_all():
+  """
+  Get All Blogposts
+  """
+  posts = BlogpostModel.get_all_blogposts()
+  data = blogpost_schema.dump(posts, many=True).data
+  return custom_response(data, 200)
+
+
